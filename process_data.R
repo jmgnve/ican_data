189b8b18fa6be8d1abd39d8c1c2ea87700c39670
jmgnve <jmg@nve.no>
2017-01-17 14:15:01 +0100

    Create dummy file names

--- process_data.R
+++ process_data.R
@@ -1,8 +1,35 @@
 
 
 
+# Paths to inputs and outputs
+
+path_prec_bil <- "dummy"
+path_tmin_bil <- "dummy"
+path_tmax_bil <- "dummy"
+path_wind_bil <- "dummy"
+
+
+# Source required files
+
+
+
+
+
+
+
 
 # Names for vic input files (coordinates from InnaforNorge.txt)  JMG
+
+df_in_norway <- data.frame(lat = c(456,654),
+                           lon = c(456,872))
+
+fn_vic_input <- c()
+
+for (irow in 1:nrow(df_in_norway)) {
+  
+  fn_vic_input[irow] <- paste("data", df_in_norway$lat[irow], df_in_norway$lon[irow], sep = "_")
+  
+}
 
 
 
