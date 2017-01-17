182b99fa581ba4227fd07f1a6f82a87329aea810
jmgnve <jmg@nve.no>
2017-01-17 15:42:15 +0100

    Output file names

--- process_data.R
+++ process_data.R
@@ -11,7 +11,7 @@ path_wind_bil <- "dummy"
 
 # Source required files
 
-
+source("vic_data_handling.R")
 
 
 
@@ -20,19 +20,21 @@ path_wind_bil <- "dummy"
 
 # Names for vic input files (coordinates from InnaforNorge.txt)  JMG
 
-df_in_norway <- data.frame(lat = c(456,654),
-                           lon = c(456,872))
+df_in_norway <- read.csv("InnenforNorge.txt", header = TRUE, sep = ";")
 
-fn_vic_input <- c()
+fn_vic_input <- vector(mode = "character", length = nrow(df_in_norway))
+id_bil_file <- vector(mode = "numeric", length = nrow(df_in_norway))
 
 for (irow in 1:nrow(df_in_norway)) {
   
-  fn_vic_input[irow] <- paste("data", df_in_norway$lat[irow], df_in_norway$lon[irow], sep = "_")
+  id_bil_file[irow] <- df_in_norway$TABID[irow]
+  
+  x_coord <- format(df_in_norway$POINT_X[irow], nsmall = 5, digits = 5)
+  y_coord <- format(df_in_norway$POINT_Y[irow], nsmall = 5, digits = 5)
+  
+  fn_vic_input[irow] <- paste("data", x_coord, y_coord, sep = "_")
   
 }
-
-
-
 
 # Open the vic files for writing + appending
 
