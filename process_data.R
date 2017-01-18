18f28a32113ec081c57bac6d8047aa000e94055a
jmgnve <jmg@nve.no>
2017-01-18 08:52:51 +0100

    Added ignore file

--- process_data.R
+++ process_data.R
@@ -8,6 +8,7 @@ path_tmin_bil <- "dummy"
 path_tmax_bil <- "dummy"
 path_wind_bil <- "dummy"
 
+path_results <- "test_data"
 
 # Source required files
 
@@ -29,14 +30,78 @@ for (irow in 1:nrow(df_in_norway)) {
   
   id_bil_file[irow] <- df_in_norway$TABID[irow]
   
-  x_coord <- format(df_in_norway$POINT_X[irow], nsmall = 5, digits = 5)
-  y_coord <- format(df_in_norway$POINT_Y[irow], nsmall = 5, digits = 5)
+  lat <- format(df_in_norway$POINT_Y[irow], nsmall = 5, digits = 5)
+  lon <- format(df_in_norway$POINT_X[irow], nsmall = 5, digits = 5)
   
-  fn_vic_input[irow] <- paste("data", x_coord, y_coord, sep = "_")
+  file_tmp <- paste("data", lat, lon, sep = "_")
+  
+  fn_vic_input[irow] <- file.path(path_results, file_tmp)
   
 }
 
-# Open the vic files for writing + appending
+# Loop over days with input data
+
+for (itime in 1:10) {
+  
+  print(itime)
+  
+  # Process minimal air temperature data
+  
+  
+  
+  
+  # Process maximum air temperature data
+  
+  
+  
+  
+  # Process wind speed data
+  
+  
+  
+  
+  
+  # Process precipitation data
+  
+  
+  
+  
+  
+  
+  
+  # Loop over grid cells
+  
+  for (ifile in 1:length(fn_vic_input)) {
+    
+    # Open vic input files for writing
+    
+    file_vic <- file(fn_vic_input[ifile], "ab")
+    
+    # Add dummy data
+    
+    prec <- 10
+    tmin <- 5
+    tmax <- 10
+    wind <- 10
+    
+    write_met_input(file_vic, prec, tmin, tmax, wind)
+    
+    close(file_vic)
+    
+  }
+  
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 ########## LOOP
